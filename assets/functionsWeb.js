97edce00936ece611123e33152ee9acda3a30f10
uafl00iZeT8k917 <45766489+aliernfrog@users.noreply.github.com>
2022-01-29 07:33:03 +0000

    Fix socials attempt

--- assets/functionsWeb.js
+++ assets/functionsWeb.js
@@ -34,7 +34,7 @@ function getPartsFromReadme(rawFile, ignore) {
   return data;
 }
 
-function loadPartsFromReadme(rawFile, div, ignore) {
+function loadPartsFromReadme(rawFile, partsRoot, ignore) {
   let parts = getPartsFromReadme(rawFile, ignore)
   let finalParts = [];
   for (i = 0; i < parts.length; i++) {
@@ -42,8 +42,8 @@ function loadPartsFromReadme(rawFile, div, ignore) {
     let partId = part.title.toLowerCase().replaceAll(" ", "-");
     finalParts.push(`<div id="${partId}" class="${partId}" style="text-align:center;color:${config.colorText}"><h3>${part.title}</h3><p1>${part.description}</p1></div>`);
   }
-  document.getElementById("root").innerHTML = finalParts.join("<br>");
-  let divs = document.getElementById("root").getElementsByTagName("div");
+  partsRoot.innerHTML = finalParts.join("<br>");
+  let divs = partsRoot.getElementsByTagName("div");
   for (i = 0; i < divs.length; i++) {
     setBgRounded(divs[i], config.colorBgPrimary, false);
   }
