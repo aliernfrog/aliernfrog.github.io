30d1fc7ce603d12d3a7e0cddf0c713794019a560
aliernfrog <45766489+aliernfrog@users.noreply.github.com>
2023-11-09 07:47:15 +0000

    feat: add home button

--- src/components/topbar.js
+++ src/components/topbar.js
@@ -1,10 +1,20 @@
 export default function({
   title,
   description,
-  icon
+  icon,
+  showHomeButton = "true"
 }) {
+  const home = showHomeButton == "true" ? [
+    `<builder-component name="chip-button"`,
+    `label="aliernfrog"`,
+    `icon="/assets/images/aliernfrog.png"`,
+    `iconSize="22"`,
+    `link="/" />`
+  ].join(" ") : "";
+  
   return [
     `<div id="topbar" style="text-align:center;">`,
+    home,
     `<div style="margin-bottom:8px;">`,
     `<img src="${icon}" style="width:64px;height:64px;vertical-align:middle;margin-right:8px;">`,
     `<p1 style="vertical-align:middle;font-size:30px;font-weight:bold;">${title}</p1><br>`,
