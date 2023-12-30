53dba0d3413b8b71b3c0c18dc21ebcf484607394
aliernfrog <45766489+aliernfrog@users.noreply.github.com>
2023-12-30 21:04:55 +0000

    feat(components/chip-button): make label bold

--- src/components/chip-button.js
+++ src/components/chip-button.js
@@ -15,7 +15,7 @@ export default function({
     link ? `<a href="${link}">` : "",
     `<div style="display:inline-block;padding:8px;margin:16px 0px 16px 0px;border-radius:25px;background-color:${containerColor};color:${contentColor};">`,
     icon ? `<img src="${icon}" alt="${label ?? ""}" style="width:${iconSize}px;height:${iconSize}px;vertical-align:middle;">` : "",
-    label ? `<p1 style="padding-left:4px;vertical-align:middle;">${label}</p1>` : "",
+    label ? `<b><p1 style="padding-left:4px;vertical-align:middle;">${label}</p1></b>` : "",
     "</div>",
     link ? "</a>" : ""
   ].join("");
