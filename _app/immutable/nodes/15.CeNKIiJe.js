fdbe52afe55a6155f4f200b19c7b50bfc519d60a
aliernfrog <aliernfrog@users.noreply.github.com>
2025-07-20 11:50:33 +0000

    deploy: deb2f04e558fe77eadd0c6a16ab2d9e7fcbf3421

---
+++ _app/immutable/nodes/15.CeNKIiJe.js
@@ -0,0 +1,27 @@
+import{t as E,a as I}from"../chunks/C9H4jhqy.js";import"../chunks/CltgQ5jN.js";import{f as N,c as d,s as n,r as c}from"../chunks/C-vpoeSO.js";import{F as O,B as R}from"../chunks/vnCglEJs.js";import{M as S}from"../chunks/CuVHFSZ9.js";import{D as T}from"../chunks/DuHn-ke5.js";const A=`By adding to a Discord Server ("server") or User Profile ("profile"), mentioning in a chat or using commands of (all referred as "interacting with") the Discord Application \`@Ms. Indinibee#6482\` (with the user ID \`698531260284534864\`, referred as "Indinibee") provided by \`aliernfrog\` ("we", "us", "our"), you agree to the Terms of Service ("terms") listed below and the [Privacy Policy](/indinibee/privacy-policy) ("policy").
+<br>
+If you disagree with any parts of the terms and/or the policy, **stop using Indinibee immediately**.
+
+## Use cases
+Indinibee can be used for entertainment purposes by interacting with it.
+<br>
+Indinibee CANNOT be used for or with the intent of:
+- violating rules and guidelines of servers
+- disturbing Discord users and other people
+- causing any kind of harm on the internet
+- posting NSFW content
+- finding or sharing exploits
+
+## Removal/limitation of access
+An user or a server's access to Indinibee can be limited or removed completely at any time, without notice or reason (reasons include violating the terms).
+
+## Liability
+YOUR USE OF INDINIBEE IS SOLELY AT YOUR OWN RISK. INDINIBEE IS PROVIDED ON AN "AS IS" AND "AS AVAILABLE" BASIS, WITH NO WARRANTIES OF ANY KIND. INDINIBEE IS OFFERED ONLY FOR ENTERTAINMENT PURPOSES AND ACCURACY, USEFULNESS, COMPLETENESS OR TIMELINESS OF ANY INFORMATION IT PROVIDES CANNOT BE WARRANTED. THEREFORE, IN NO EVENT WILL INDINIBEE, DISCORD OR WE BE LIABLE FOR ANY CONSEQUENTIAL, INCIDENTAL, INDIRECT OR SPECIAL DAMAGES WHATSOEVER (INCLUDING LOSS OF DATA) ARISING OUT OF THE USE OR INABILITY TO USE INDINIBEE.
+<br>
+WE DO NOT WARRANT THAT THE SERVICE WILL FUNCTION UNITERRUPTED, SECURE OR AVAILABLE AT ANY PARTICULAR TIME OR LOCATION; ANY ERRORS OR DEFECTS WILL BE CORRECTED; INDINIBEE IS FREE OF VIRUSES OR OTHER HARMFUL COMPONENTS; OR THE RESULTS OF USING INDINIBEE WILL MEET YOUR REQUIREMENTS.
+
+## Changes to Terms and Policy
+The terms and the policy can be changed at any time, without notice.
+
+## Contact us
+If you have any questions regarding the terms or the policy, contact us at [our Discord server](https://aliernfrog.github.io/discord).`;var p=E('<!> <div class="fancya"><!></div> <!> <!>',1);function b(t){var i=p(),o=N(i);O(o,{title:"Terms of Service",sideTitle:"of Ms. Indinibee",description:"Last updated: November 13th, 2024",icon:"/icons/projects/indinibee.png",iconLink:"/indinibee",backButton:{label:"Ms. Indinibee",icon:"/icons/projects/indinibee.png",url:"/indinibee"},showHomeButton:!0});var e=n(o,2),s=d(e);S(s,{text:A}),c(e);var r=n(e,2);T(r,{});var a=n(r,2);R(a),I(t,i)}export{b as component};
---
+++ _app/immutable/nodes/3.DDjUej1m.js
@@ -0,0 +1,1 @@
+import"../chunks/C9H4jhqy.js";import"../chunks/CltgQ5jN.js";import{R as t}from"../chunks/CzTt7KKk.js";function e(o){t(o,{url:"/posts/android-data-access"})}export{e as component};
